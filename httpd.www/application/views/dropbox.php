216aafce29f482e3cdb80c9c27a9e76875c5e2f4
Thomas <thomas.kramp378@gmail.com>
2022-12-18 00:04:56 +0100

    updates

--- httpd.www/application/views/dropbox.php
+++ httpd.www/application/views/dropbox.php
@@ -3,51 +3,55 @@
     <div class="grid">
       <h1 style="text-align: center;">Onze partners</h1>
       <div id="slideshow">
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/vinci_energies.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/itb.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/chipsoft.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/psa_antwerp.png" /></div>
 
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/star_power_people.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/umicore.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/dp_world.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/camco.png" /></div>
 
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/denis_engine.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/remotive.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/rent_solutions.png" /></div>
 
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/star_power_people.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/volvo_cars.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/deloitte.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/dosign.png" /></div>
 
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/dosign.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/aurubis_olen.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/axxes.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/talenco.png" /></div>
 
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/talenco.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/jan_de_nul.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/tormans.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/aertssen_group.png" /></div>
 
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/aertssen_group.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/botec_nv.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/xplore_group.png" /></div>
+
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/prevom.png" /></div>
-
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/air_liquide.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/nexios_it.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/smulders.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/air_liquide.png" /></div>
 
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/fritkot_max.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/basf.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/ats_groep.png" /></div>
-
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/carrefour.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/8tea5.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/studant.png" /></div>
-
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/nexios_it.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/sick.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/sweco.png" /></div>
-        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/gecon.png" /></div>
 
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/gecon.png" /></div>
         <div><img src="<?php echo base_url(); ?>assets/images/sponsors/rp_one.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/clayton.png" /></div>
+
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/devoteam.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/fritkot_max.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/basf.png" /></div>
+
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/ats_groep.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/carrefour.png" /></div>
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/8tea5.png" /></div>
+        
+        <div><img src="<?php echo base_url(); ?>assets/images/sponsors/studant.png" /></div>
       </div>
       <p style="text-align: center;">
         Klik hiernaast <a href="https://www.dropbox.com/sh/jph775hz85epmkr/AADbc3p1jESUHQ8oQ8qlx_ZVa?dl=0" target="_blank">(Dropbox Technica)</a>. Het wachtwoord is te verkrijgen bij het Technica praesidium.
